eb68183b175a291d1f93c3feaa7775a07aa818f3
Alex Miller <alexmiller422@gmail.com>
2024-03-04 20:51:00 +0000

    Add Scalafmt
    Squashed commit of the following:
    
    commit 7e141fb65d5f26ba33567aaadec09f653618f389
    Author: Alex Miller <alexmiller422@gmail.com>
    Date:   Mon Mar 4 20:39:03 2024 +0000
    
        Ensure code adheres to Scalafmt standards
    
    commit 53a7331f11969ccdc17b2d6025d553b9838896c5
    Author: Alex Miller <alexmiller422@gmail.com>
    Date:   Mon Mar 4 20:17:05 2024 +0000
    
        Add Scalafmt to CI builds

--- domain/src/main/scala/io/sportsmrm/teamsheets/domain/TeamSheetState.scala
+++ domain/src/main/scala/io/sportsmrm/teamsheets/domain/TeamSheetState.scala
@@ -9,7 +9,6 @@ import org.apache.pekko.persistence.typed.scaladsl.Effect
 import java.time.LocalDate
 import java.util.UUID
 
-
 sealed trait TeamSheetState {
   def applyCommand(command: Command[?]): ReplyEffect
   def applyEvent(event: Event): TeamSheetState
@@ -19,7 +18,8 @@ case object EmptyTeamSheet extends TeamSheetState {
   override def applyCommand(command: Command[?]): ReplyEffect = {
     command match {
       case CreateTeamSheet(id, date, team, opponent, replyTo) =>
-        Effect.persist(TeamSheetCreated(id, date, team, opponent))
+        Effect
+          .persist(TeamSheetCreated(id, date, team, opponent))
           .thenReply(replyTo)(_ => StatusReply.Ack)
       case _ =>
         Effect.unhandled.thenNoReply()
@@ -28,14 +28,22 @@ case object EmptyTeamSheet extends TeamSheetState {
 
   override def applyEvent(event: Event): TeamSheetState =
     event match {
-      case TeamSheetCreated(id, date, team, opponent) => CreatedTeamSheet(id, date, team, opponent)
-      case _ => throw new IllegalStateException(s"unsupported event [$event] in state [EmptyTeamSheet]")
+      case TeamSheetCreated(id, date, team, opponent) =>
+        CreatedTeamSheet(id, date, team, opponent)
+      case _ =>
+        throw new IllegalStateException(
+          s"unsupported event [$event] in state [EmptyTeamSheet]"
+        )
     }
 }
 
-case class CreatedTeamSheet(uuid: UUID, date: LocalDate, team: Team, opponent: Opponent) extends TeamSheetState {
+case class CreatedTeamSheet(
+    uuid: UUID,
+    date: LocalDate,
+    team: Team,
+    opponent: Opponent
+) extends TeamSheetState {
   override def applyCommand(command: Command[?]): ReplyEffect = ???
 
   override def applyEvent(event: Event): TeamSheetState = ???
 }
-
--- domain/src/main/scala/io/sportsmrm/teamsheets/domain/package.scala
+++ domain/src/main/scala/io/sportsmrm/teamsheets/domain/package.scala
@@ -3,5 +3,8 @@ package io.sportsmrm.teamsheets
 import io.sportsmrm.teamsheets.events.Event
 
 package object domain {
-  type ReplyEffect = org.apache.pekko.persistence.typed.scaladsl.ReplyEffect[Event, TeamSheetState]
+  type ReplyEffect = org.apache.pekko.persistence.typed.scaladsl.ReplyEffect[
+    Event,
+    TeamSheetState
+  ]
 }
